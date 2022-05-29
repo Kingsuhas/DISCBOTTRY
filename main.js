32fcf6e7ce3b93978d4e5f5e738d6dddb801a2cb
Kingsuhas <OG.suhas@gmail.com>
2022-05-29 23:04:47 +0530

    it is alive
    finally replies

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents} = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -6,8 +6,10 @@ client.on('ready' , () => {
     console.log('GAbot is online');
 });
 
-client.on('message' , (message) =>{
-    message.reply('meow');
-});
+client.on('messageCreate' , msg =>{
+        if (msg.content == ".name"){
+            msg.reply(msg.author.username);
+        }
+    });
 
 client.login('OTc2NDExMzI2NjE0MjE2NzA0.GJQCDb.boodbR9CSzwZ5v9aY7S78p2lqGwZ8UQUc5r8PM');
